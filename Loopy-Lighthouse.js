bde4789fd0aa8c7c4bbbc0fc2a9af04fa638e143
Jocelyne <lyne1604@gmail.com>
2019-03-20 16:51:20 +0000

    Loopy-Lighthouse assignement completed

---
+++ Loopy-Lighthouse.js
@@ -0,0 +1,12 @@
+var count = 200;
+for(var i = 100; i <= count; i++){
+     if(i % 3 == 0 && i % 4 == 0){
+      console.log("LoopyLighthouse");
+    }else if(i % 3 == 0){
+      console.log("Loopy");
+    }else if(i % 4 == 0){
+      console.log("Lighthouse");
+    }else{
+  console.log(i);
+    }
+}
